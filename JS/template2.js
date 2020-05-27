a9c904ad3cc0a781cc1df8739ad501efd0f7dac3
linman5 <63662675+linman5@users.noreply.github.com>
2020-05-27 17:21:31 +0100

    Add files via upload

---
+++ JS/template2.js
@@ -0,0 +1,27 @@
+/**
+* TEMPLATE 2 JAVASCRIPT
+*/
+
+// Change navbar background color on scroll
+window.onscroll = function() {
+    const navbar = document.querySelector('.navbar2')
+
+    // pageYOffset or scrollY
+    if (window.pageYOffset > 0) {
+        navbar.classList.add('scrolled')
+    } else {
+        navbar.classList.remove('scrolled')
+    }
+}
+
+
+
+var figure = $(".video").hover( hoverVideo, hideVideo );
+
+function hoverVideo(e) {  
+    $('video', this).get(0).play(); 
+}
+
+function hideVideo(e) {
+    $('video', this).get(0).pause(); 
+}
